06f0884638c80ca56dfd68cf54ddb36bcbb97fc2
GabrielDNeto <gabrielneto.contato@gmail.com>
2021-08-27 18:37:40 -0300

    Routes update

--- src/routes/login_register.js
+++ src/routes/login_register.js
@@ -3,14 +3,38 @@ const router = express.Router();
 const User = require('../models/User')
 
 router.get('/users', async (req, res) => {
-  const ListOfAllUsers = await User.findAll();
-  res.send(ListOfAllUsers);
+  try {
+    const ListOfAllUsers = await User.findAll();
+    res.send(ListOfAllUsers);
+  } catch (error) {
+    res.status(400).send({error: 'Cannot find User list'})
+  }
 })
 
 router.post('/register', async (req, res) => {
-  const user = req.body;
-  await User.create(user)
-  res.send(user)
+  try {
+    const user = req.body;
+    await User.create(user)
+    res.send(user)
+  } catch (error) {
+    res.status(400).send({error: 'Cannot register new user'})
+  }
+})
+
+router.post('/login', async (req, res) => {
+  const { email, password } = req.body;
+  try {
+    const user = User.findOne({ email, password})
+
+    if(!user) {
+      return res.send(401)
+    }
+
+    res.send(user)
+
+  } catch (error) {
+    res.status(401).send({error: 'Login Failed'})
+  }
 })
 
 module.exports = router;
